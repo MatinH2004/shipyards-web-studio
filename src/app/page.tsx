2f9d6cde1ff28f8279986d9191ff2b88bd8df734
Matin Hassan Pour <matinhp04@gmail.com>
2025-04-03 14:13:06 -0700

    update component import paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Features from "@/components/Features";
-import Offer from "@/components/Offer";
-import Portfolio from "@/components/Portfolio";
-import Pricing from "@/components/Pricing";
-import Footer from "@/components/Footer";
+import Hero from "@/components/home/Hero";
+import Navbar from "@/components/global/Navbar";
+import Features from "@/components/home/Features";
+import Offer from "@/components/home/Offer";
+import Portfolio from "@/components/home/Portfolio";
+import Pricing from "@/components/home/Pricing";
+import Footer from "@/components/global/Footer";
 
 export default function HomePage() {
   return (
