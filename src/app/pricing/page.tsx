f4eec2ec3f1f30ec165f278c816e7d92014d5384
Matin Hassan Pour <matinhp04@gmail.com>
2025-04-04 15:47:09 -0700

    pricing page

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/global/Navbar";
+import Footer from "@/components/global/Footer";
+import Pricing from "@/components/home/Pricing";
+
+export default function AboutPage() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Pricing />
+      </main>
+      <Footer />
+    </>
+  );
+}
