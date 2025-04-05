7e1deebfc8a4d9e16e9ca787c339aad54557a335
Matin Hassan Pour <matinhp04@gmail.com>
2025-04-04 22:46:31 -0700

    add favicon files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,16 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Shipyards Web Studio",
   description: "Small Business Web Designer | Vancouver, BC, Canada",
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon.png', sizes: '180x180' }
+    ],
+  },
+  manifest: '/favicon/site.webmanifest',
 };
 
 export default function RootLayout({
